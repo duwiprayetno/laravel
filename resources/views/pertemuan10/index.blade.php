984c901cf15203085502402d1a12c5b1676d5eb4
duwiprayetno <dwiprayetno15.dp@gmail.com>
2023-12-16 04:11:08 +0700

    tgs1pertemuan10

--- resources/views/pertemuan10/index.blade.php
+++ resources/views/pertemuan10/index.blade.php
@@ -10,7 +10,7 @@
 <body>
     <a href="/create">tambah mahasiswa</a>
     <h1>mahasiswa</h1>
-    <h2>Duwi</h2>
+    <h2>Duwi2</h2>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
